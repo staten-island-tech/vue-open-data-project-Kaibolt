72fdf99e3b5a3a9857522aa160746969ad44c059
Kaibolt <kyle.salowitz08@gmail.com>
2025-03-03 12:01:09 -0500

    Create TheToilets.vue

---
+++ app/src/components/TheToilets.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup>
+function getToilets() {
+    let res = await fetch('https://data.cityofnewyork.us/resource/hjae-yuav.json')
+    let data = await res.json
+    console.log(data.results)
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
