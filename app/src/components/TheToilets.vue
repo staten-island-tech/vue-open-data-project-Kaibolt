841b81efc96f19cd507d2233b56d3b2a33d8b56f
Kaibolt <kyle.salowitz08@gmail.com>
2025-03-04 12:31:48 -0500

    Trying to make a variable from an async function
    I feel sick

--- app/src/components/TheToilets.vue
+++ app/src/components/TheToilets.vue
@@ -9,9 +9,20 @@
 async function getToilets() {
     let res = await fetch(`https://data.cityofnewyork.us/resource/hjae-yuav.json`);
     let toiletData = await res.json();
-    console.log(toiletData);
+    return toiletData // after checking with console.log, the toilet data indeed exists
+}
+async function makeNames() {
+    let toiletData = await getToilets()
+    let i = 0
+    let namedToilets = [];
+    while (i < toiletData.length) {
+        namedToilets.push(toiletData[i].name)
+        i++
+    } //After checking with console.log, the toilet names are indeed pushed
+    return namedToilets
 }
 getToilets()
+let test = async
 </script>
 
 <style lang="scss" scoped>
