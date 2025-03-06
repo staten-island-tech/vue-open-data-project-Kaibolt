f9aee690a1491b2bcc864bca2bc0632799793626
Kaibolt <kyle.salowitz08@gmail.com>
2025-03-06 11:38:48 -0500

    Plugin Imports
    Tailwind CSS imported
    Daisy UI imported..? I think.

--- app/src/components/TheToilets.vue
+++ app/src/components/TheToilets.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
         <h2>TheToilets.vue is in use.</h2>
-        <div v-for="toilet in toiletArray">
+        <div v-for="toilet in toiletArray"> <!--Make this a card later-->
 
-            <h3 > {{ toilet.name }}</h3>
-            <h4>{{ toilet.location }}</h4>
-            <p>{{ toilet.borough }}</p>
+            <h3> {{ toilet.name }}</h3> <!--Name of the toilet area-->
+            <h4>{{ toilet.location }}</h4> <!--The location of the toilet-->
+            <p>{{ toilet.borough }}</p> <!--The borough the toilet is in-->
 
         </div>
     </div>
