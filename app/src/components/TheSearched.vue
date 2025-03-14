cd4f69b6e713753ce5adb84e9f516589a3353988
Kaibolt <kyle.salowitz08@gmail.com>
2025-03-14 12:32:23 -0400

    Create TheSearched.vue

---
+++ app/src/components/TheSearched.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="flex flex-wrap">
+      <div
+        class="m-8 card bg-base-100 w-70 shadow-xl"
+        v-for="toilet in searchedArray"
+      >
+        <!--Start of the card-->
+        <div class="card-body">
+          <h2 class="card-title">{{ toilet.name }}</h2>
+          <h3>{{ toilet.location }}</h3>
+          <h4>{{ toilet.borough }}</h4>
+        </div>
+      </div>
+      <!--End of the card-->
+    </div>
+  </div>
+</template>
+
+<script setup>
+async function getToilets() {
+  let res = await fetch(
+    `https://data.cityofnewyork.us/resource/hjae-yuav.json`
+  );
+  let toiletData = await res.json();
+  return toiletData; // after checking with console.log, the toilet data indeed exists
+}
+let toiletArray = await getToilets().then(); // .then() wont work, neither will Promise.resolve(test)
+
+function searchTest(user) {
+    i = 0
+    while (i < toiletArray.length) {
+        if (user in toiletArray[i].name) {
+            console.log(toiletArray[i])
+        }
+        i++
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
