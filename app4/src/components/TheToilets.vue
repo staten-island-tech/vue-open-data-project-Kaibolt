9227fd795be362e4ff1d44b6e1c89c3fe85e57fa
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2025-03-24 11:47:03 -0400

    scrapped dropdown, wouldn't work

--- app4/src/components/TheToilets.vue
+++ app4/src/components/TheToilets.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
+    <!--    <div class="dropdown">
+      <div tabindex="0" role="button" class="btn m-1">Boroughs</div>
+      <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+        <li><a>All</a></li>
+        <li><a @click="filterToilets('brooklyn')">brooklyn</a></li>
+        <li><a>bronx</a></li>
+        <li><a>queens</a></li>
+        <li><a>manhatten</a></li>
+        <li><a>staten island</a></li>
+      </ul>
+    </div>-->
     <div class="flex flex-wrap">
       <div class="m-8 card bg-base-100 w-70 shadow-xl" v-for="toilet in toiletArray">
         <!--Start of the card-->
@@ -21,7 +32,17 @@ async function getToilets() {
   return toiletData // after checking with console.log, the toilet data indeed exists
 }
 let toiletArray = await getToilets().then() // .then() wont work, neither will Promise.resolve(test)
-console.log(toiletArray)
+/*
+function filterToilets(borough) {
+  while (((x = 0), x < toiletArray.length)) {
+    if (toiletArray[x].borough == borough) {
+      console.log(toiletArray[x])
+    }
+    x++
+  }
+}
+
+*/
 </script>
 
 <style lang="scss" scoped></style>
