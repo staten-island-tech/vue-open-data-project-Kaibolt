739243cc5d7d5dbb8857f7d2c964ead50f88e601
Kaibolt <144480358+Kaibolt@users.noreply.github.com>
2025-03-25 21:36:31 -0400

    wtf of 1984

--- app4/src/components/TheToilets.vue
+++ app4/src/components/TheToilets.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <!--    <div class="dropdown">
+    <div class="dropdown">
       <div tabindex="0" role="button" class="btn m-1">Boroughs</div>
       <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
-        <li><a>All</a></li>
-        <li><a @click="filterToilets('brooklyn')">brooklyn</a></li>
-        <li><a>bronx</a></li>
+        <li><a @click="testClick($event)">All</a></li>
+        <li><a @click="testClick($event)">Brooklyn</a></li>
+        <li><a @click="testClick($event)">Bronx</a></li>
         <li><a>queens</a></li>
         <li><a>manhatten</a></li>
         <li><a>staten island</a></li>
       </ul>
-    </div>-->
+    </div>
     <div class="flex flex-wrap">
-      <div class="m-8 card bg-base-100 w-70 shadow-xl" v-for="toilet in toiletArray">
+      <div class="m-8 card bg-base-100 w-70 shadow-xl" v-for="toilet in finalArray">
         <!--Start of the card-->
         <div class="card-body">
           <h2 class="card-title">{{ toilet.name }}</h2>
@@ -26,23 +26,27 @@
 </template>
 
 <script setup>
-async function getToilets() {
-  let res = await fetch(`https://data.cityofnewyork.us/resource/hjae-yuav.json`)
-  let toiletData = await res.json()
-  return toiletData // after checking with console.log, the toilet data indeed exists
+import { toiletArray } from '@/assets/general.js'
+import { ref } from 'vue'
+let selectedBorough = ref('')
+let finalArray = ref(toiletArray)
+function testClick(event) {
+  selectedBorough = event.target.innerHTML
+  arrayMaker()
+  console.log(finalArray.value)
 }
-let toiletArray = await getToilets().then() // .then() wont work, neither will Promise.resolve(test)
-/*
-function filterToilets(borough) {
-  while (((x = 0), x < toiletArray.length)) {
-    if (toiletArray[x].borough == borough) {
-      console.log(toiletArray[x])
-    }
-    x++
+function arrayMaker() {
+  let finalArray = []
+  if (selectedBorough.value == 'All' || selectedBorough.value == '') {
+    finalArray.value = toiletArray
+    console.log(finalArray.value)
+  }
+  if (selectedBorough.value !== 'All' || selectedBorough.value !== '') {
+    console.log(selectedBorough.value)
+    finalArray.value = toiletArray.filter((toilet) => toilet.borough === selectedBorough.value)
+    console.log(finalArray.value)
   }
 }
-
-*/
 </script>
 
 <style lang="scss" scoped></style>
